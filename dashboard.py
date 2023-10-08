dfa1beced3f08c9b24deec44d9339d2b8d11b15e
Andy Chen <andy.chen121@imperial.ac.uk>
2023-10-09 00:43:29 +0100

    Working html and api

--- dashboard.py
+++ dashboard.py
@@ -4,7 +4,13 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST']) # show the main page
 def index():
+    print(request.form)
     return render_template('index.html')
+
+@app.route('/maps', methods=['GET', 'POST']) # show the main page
+def maps():
+    loc = request.form.get('location')
+    return render_template('maps.html', location = loc, url="https://google.com/maps/place/"+loc)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5005, debug=True)
